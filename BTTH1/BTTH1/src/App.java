88075d4e9b5d7832233597dbdd5987195b074cbb
vuong2002 <97439077+vuong2002@users.noreply.github.com>
2022-01-17 19:26:31 +0700

    first commit

---
+++ BTTH1/BTTH1/src/App.java
@@ -0,0 +1,14 @@
+nhập  java.util.Scanner ;
+ lớp  công khai bai1 {
+    public  static  void  main ( String [] args ) ném  Exception {
+        // doi tuong de nhap xuat
+        Máy quét sc =  Máy quét mới  ( Hệ thống . In);
+        // khai bao bien de chua gia tri nhap vao
+        Chuỗi strHT =  " " ;
+        // thong bao dau vao
+        Hệ thống . ra ngoài . println ( " Nhap ho ten: " );
+        // gan gia tri tu ban phim
+        strHT = sc . hàng tiếp theo();
+        Hệ thống . ra ngoài . println ( " Xin chao "  + strHT);
+    }
+}
