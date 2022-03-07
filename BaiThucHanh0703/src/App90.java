cc4d82499602cb19da14b767587acdc6325e4997
vuong2002 <97439077+vuong2002@users.noreply.github.com>
2022-03-07 17:54:36 +0700

    PhamNgocVuong

---
+++ BaiThucHanh0703/src/App90.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class App90 {
+  public static void main(String[] args) {
+    String chuoi;
+    char kyTu;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nhập vào chuỗi bất kỳ: ");
+    chuoi = scanner.nextLine();
+    System.out.println("Các ký tự có trong chuỗi là: ");
+      for (int i = 0; i < chuoi.length(); i++) {
+          kyTu = chuoi.charAt(i);
+          System.out.println(kyTu);
+      }
+  }
+}
